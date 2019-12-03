02f503f884551305b3c5ce36cef82f22aca3604f
Aguilar Dias <aguilar.dias@serpro.gov.br>
2019-12-03 11:31:36 -0200

    swagger doc

--- src/main/java/br/com/codenation/softlog/dto/request/LoginDTO.java
+++ src/main/java/br/com/codenation/softlog/dto/request/LoginDTO.java
@@ -1,10 +1,14 @@
 package br.com.codenation.softlog.dto.request;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
 public class LoginDTO {
 
+	@ApiModelProperty(example = "joao@codenation.com")
 	private String email;
+	
+	@ApiModelProperty(example = "codenation")
 	private String password;
 }
